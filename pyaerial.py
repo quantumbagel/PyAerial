e44505a4fc1f8ab7075d19ba9ffecb7265b03f6e
quantumbagel <jjreder@gmail.com>
2024-03-14 19:30:40 -0400

    PyAerial development starts now basic testing data is set up

--- pyaerial.py
+++ pyaerial.py
@@ -1,8 +1,19 @@
+import math
 import threading
 import time
 import pyModeS as pms
 import signal_generator
-from signal_generator import message_queue as messages
+import ruamel.yaml
+import geopy.distance
+from shapely.geometry import Point, Polygon
+
+STORE_INFO = "info"
+STORE_RECV_DATA = "received_data"
+STORE_CALC_DATA = "calculated_data"
+MAX_SPEED_CALC_BACK_PACKET = 10
+BACKDATE_HORIZONTAL_SPEED = 10
+REMEMBER_PLANES = 60
+POINT_ACCURACY_THRESHOLD_DEG = 10
 
 
 def initate_signal_generator():
@@ -11,8 +22,20 @@ def initate_signal_generator():
     return signal_thread
 
 
-lat_h = 35.6810752
-lon_h = -78.8758528
+# https://stackoverflow.com/questions/33311616/find-coordinate-of-the-closest-point-on-polygon-in-shapely
+def load_configuration():
+    yaml = ruamel.yaml.YAML()
+    data = yaml.load(open('config.yaml'))
+    home = data['home']
+    home_lat = home['latitude']
+    home_lon = home['longitude']
+    general = {'home': [home_lat, home_lon]}
+
+    return general, data['zones'], data['categories']
+
+
+configuration, zones, categories = load_configuration()
+
 generator_thread = initate_signal_generator()
 
 categories = {2: {1: "Surface Emergency Vehicle", 3: "Surface Service Vehicle", 4: "Ground Obstruction (4)",
@@ -41,21 +64,22 @@ def classify(msg):
     typecode_category = "Unknown"
     if 1 <= typecode <= 4:  # Aircraft identification
         ca = pms.adsb.category(msg)
-        data = {"info": {"icao": icao, "callsign": pms.adsb.callsign(msg).replace("_", ""),
-                         "category": [typecode, ca]}, "data": {}}
+        data = {STORE_INFO: {"icao": icao, "callsign": pms.adsb.callsign(msg).replace("_", ""),
+                             "category": [typecode, ca]}, STORE_RECV_DATA: {}}
         typecode_category = 1
 
     elif 5 <= typecode <= 8:  # Surface position
-        lat, lon = pms.adsb.position_with_ref(msg, lat_h, lon_h)
+        lat, lon = pms.adsb.position_with_ref(msg, configuration['home'][0], configuration['home'][1])
         speed, angle, vert_rate, speed_type, angle_source, vert_rate_source = pms.adsb.velocity(msg, source=True)
-        data = {"info": {"icao": icao}, "data": {"latitude": lat, "longitude": lon, "horizontal_speed": speed * 1.852,
-                                                 "direction": angle, "vertical_speed": vert_rate * 0.018288}}
+        data = {STORE_INFO: {"icao": icao},
+                STORE_RECV_DATA: {"latitude": lat, "longitude": lon, "horizontal_speed": speed * 1.852,
+                                  "direction": angle, "vertical_speed": vert_rate * 0.018288}}
         typecode_category = 2
 
     elif 9 <= typecode <= 18 or 20 <= typecode <= 22:  # Airbone position (baro alt/GNSS alt)
-        lat, lon = pms.adsb.position_with_ref(msg, lat_h, lon_h)
+        lat, lon = pms.adsb.position_with_ref(msg, configuration['home'][0], configuration['home'][1])
         alt = pms.adsb.altitude(msg)
-        data = {"info": {"icao": icao}, "data": {"latitude": lat, "longitude": lon, "altitude": alt}}
+        data = {STORE_INFO: {"icao": icao}, STORE_RECV_DATA: {"latitude": lat, "longitude": lon, "altitude": alt}}
         if 9 <= typecode <= 18:
             typecode_category = 3
         else:
@@ -63,8 +87,9 @@ def classify(msg):
 
     elif typecode == 19:  # Airbone velocities
         speed, angle, vert_rate, speed_type, angle_source, vert_rate_source = pms.adsb.velocity(msg, source=True)
-        data = {"info": {"icao": icao},
-                "data": {"horizontal_speed": speed * 1.852, "direction": angle, "vertical_speed": vert_rate * 0.018288}}
+        data = {STORE_INFO: {"icao": icao},
+                STORE_RECV_DATA: {"horizontal_speed": speed * 1.852, "direction": angle,
+                                  "vertical_speed": vert_rate * 0.018288}}
         typecode_category = 5
 
     elif typecode == 28:  # Aircraft status
@@ -73,12 +98,13 @@ def classify(msg):
         pass
     elif typecode == 31:  # Aircraft operation status
         pass  # Not going to implement this type of message
-
+    if data is not None:
+        data.update({STORE_CALC_DATA: {}})
     return data, typecode_category
 
 
-def get_newest(packets):
-    return packets[-1]
+def check_should_be_added(packets, new_packet):
+    return packets[-1][0] != new_packet[0]
 
 
 def check_generator_thread():
@@ -89,51 +115,215 @@ def check_generator_thread():
         print("Restarting generator thread...")
         generator_thread = initate_signal_generator()
 
-planes = {}
-processed_messages = 0
-while True:
-    check_generator_thread()
-    messages_copy = messages[:]
-    for message in messages_copy:
-        data, typecode_cat = classify(message[0])
-        if data is None:  # Not implemented yet :(
+
+def calculate_heading(previous, current):
+    """
+    Calculate the direction of the heading from the lat long pair previous to the lat long pair current.
+    :param previous: The previous position (lat/long)
+    :param current: The current position (lat/long)
+    :return: The heading (degrees)
+    """
+
+    pi_c = math.pi / 180
+    first_lat = previous[0] * pi_c
+    first_lon = previous[1] * pi_c
+    second_lat = current[0] * pi_c
+    second_lon = current[1] * pi_c
+    y = math.sin(second_lon - first_lon) * math.cos(second_lat)
+    x = (math.cos(first_lat) * math.sin(second_lat)) - (
+            math.sin(first_lat) * math.cos(second_lat) * math.cos(second_lon - first_lon))
+    heading_rads = math.atan2(y, x)
+    heading_degs = ((heading_rads * 180 / math.pi) + 360) % 360
+    return heading_degs
+
+
+def calculate_speed(previous, current, previous_time, current_time):
+    dist_xz = geopy.distance.geodesic(previous, current).m
+    elapsed_time = current_time - previous_time
+    speed = dist_xz / elapsed_time * 3.6  # m/s to km/h
+    return speed
+
+
+def patch_append(plane, category, message_type, message):
+    if message_type in planes[plane][category].keys():
+        planes[plane][category][message_type].append(message)
+        return True
+    else:
+        planes[plane][category][message_type] = [message]
+        return True
+    # return False
+
+
+def process_messages(msgs):
+    global planes
+    processed = 0
+    for message in msgs:
+        message_data, typecode_cat = classify(message[0])
+        if message_data is None:  # Not implemented yet :(
             continue
-        processed_messages += 1
-        if data["info"]["icao"] not in planes.keys():  # Do we have this plane in our current tracker?
-            planes[data["info"]["icao"]] = data  # We don't, so just format the data and insert it
-            for item in planes[data["info"]["icao"]]["data"]:
-                c_item = planes[data["info"]["icao"]]["data"][item]
-                planes[data["info"]["icao"]]["data"][item] = [[c_item, message[1]]]
+        processed += 1
+        if message_data[STORE_INFO]["icao"] not in planes.keys():  # Do we have this plane in our current tracker?
+            planes[message_data[STORE_INFO]["icao"]] = message_data  # We don't, so just format the data and insert it
+            for item in planes[message_data[STORE_INFO]["icao"]][STORE_RECV_DATA]:
+                c_item = planes[message_data[STORE_INFO]["icao"]][STORE_RECV_DATA][item]
+                planes[message_data[STORE_INFO]["icao"]][STORE_RECV_DATA][item] = [[c_item, message[1]]]
         else:  # We do, so find and replace (or update) data
-            current_info = planes[data["info"]["icao"]]["info"]  # Current plane info
-            my_info = data["info"]
+            current_info = planes[message_data[STORE_INFO]["icao"]][STORE_INFO]  # Current plane info
+            my_info = message_data[STORE_INFO]
             for item in my_info.keys():  # Find and replace/update data
                 if item not in current_info.keys():
                     current_info.update({item: my_info[item]})
                 else:
                     current_info[item] = my_info[item]
-            planes[data["info"]["icao"]]["info"] = current_info
-            current_data = planes[data["info"]["icao"]]["data"]  # Similar thing here, but using lists
-            for datum in data["data"].keys():
-                compiled = [data["data"][datum], message[1]]
+            planes[message_data[STORE_INFO]["icao"]][STORE_INFO] = current_info
+            current_data = planes[message_data[STORE_INFO]["icao"]][STORE_RECV_DATA]
+            # Similar thing here, but using lists
+            for datum in message_data[STORE_RECV_DATA].keys():
+                new_packet = [message_data[STORE_RECV_DATA][datum], message[1]]
                 if datum not in current_data.keys():
-                    current_data.update({datum: [compiled]})
+                    current_data.update({datum: [new_packet]})
                 else:
-                    current_data[datum].append(compiled)
+                    if check_should_be_added(current_data[datum], new_packet):
+                        current_data[datum].append(new_packet)
 
-        if "internal" not in planes[data["info"]["icao"]].keys():  # Update internal metrics
-            planes[data["info"]["icao"]].update({"internal": {"last_update": message[1], "packets": 1,
-                                                              "first_packet": message[1], "packet_type":
-            {typecode_cat: 1}}})
+        if "internal" not in planes[message_data[STORE_INFO]["icao"]].keys():  # Update internal metrics
+            planes[message_data[STORE_INFO]["icao"]].update({"internal": {"last_update": message[1], "packets": 1,
+                                                                          "first_packet": message[1], "packet_type":
+                                                                              {typecode_cat: 1}}})
         else:
-            internal_data_storage = planes[data["info"]["icao"]]["internal"]
+            internal_data_storage = planes[message_data[STORE_INFO]["icao"]]["internal"]
             internal_data_storage["last_update"] = message[1]
             internal_data_storage["packets"] += 1
             if typecode_cat in internal_data_storage["packet_type"].keys():
                 internal_data_storage["packet_type"][typecode_cat] += 1
             else:
                 internal_data_storage["packet_type"][typecode_cat] = 1
-        messages.pop(0)
-        if processed_messages % 10 == 0:
-            print(processed_messages, planes)
+    return processed
+
+
+def calculate():
+    for plane in planes:
+        if "latitude" not in planes[plane][STORE_RECV_DATA].keys():  # Haven't yet received latitude/longitude packet
+            continue
+        latitude_data = planes[plane][STORE_RECV_DATA]["latitude"]
+        longitude_data = planes[plane][STORE_RECV_DATA]["longitude"]
+        if len(latitude_data) == 1:  # Need at least two lat/long pairs
+            continue
+        else:
+            if len(latitude_data) < MAX_SPEED_CALC_BACK_PACKET:  # If we don't have at least the max calc back packets,
+                # set the previous to the first packet
+                previous_lat = latitude_data[0]
+                previous_lon = longitude_data[0]
+
+            else:
+                old_packet = len(latitude_data) - MAX_SPEED_CALC_BACK_PACKET  # Find the indice of the old packet
+                previous_lat = latitude_data[old_packet]
+                previous_lon = longitude_data[old_packet]
+            previous = [previous_lat[0], previous_lon[0]]  # Previous lat/long
+            previous_time = previous_lat[1]
+            current_lat = latitude_data[-1]
+            current_lon = longitude_data[-1]
+            current = [current_lat[0], current_lon[0]]  # Current lat/long
+            current_time = current_lat[1]
+            speed = calculate_speed(previous, current, previous_time, current_time)  # Calculate speed
+            heading = calculate_heading(previous, current)  # Calculate heading
+            if "horizontal_speed" not in planes[plane][STORE_RECV_DATA].keys():
+                # If we haven't received a horiz_speed packet, don't even consider using it
+                final_speed = speed
+                speed_time = current_time
+            else:
+                horiz_plane_speed = planes[plane][STORE_RECV_DATA]["horizontal_speed"][-1]
+                # Otherwise, get the time of the newest one
+                time_ago = current_time - horiz_plane_speed[1]
+                if time_ago < MAX_SPEED_CALC_BACK_PACKET:  # Is it new enough to be relevant?
+                    final_speed = horiz_plane_speed[0]  # Use it
+                    speed_time = horiz_plane_speed[1]
+                else:
+                    final_speed = speed  # Not relevant, use computed speed
+                    speed_time = current_time
+
+            # Add speed to data
+            patch_append(plane, STORE_CALC_DATA, "horizontal_speed", [final_speed, speed_time])
+
+            if "heading" not in planes[plane][STORE_RECV_DATA].keys():  # If we don't have heading data
+                final_heading = heading  # Use computed heading
+            else:
+                heading_data = planes[plane][STORE_RECV_DATA]["heading"][-1]  # Get newest heading data
+                time_ago = current_time - heading_data[1]
+                if time_ago < MAX_SPEED_CALC_BACK_PACKET:  # Is it new enough?
+                    final_heading = heading_data[0]  # Use it
+                else:
+                    final_heading = heading  # Not relevant
+            patch_append(plane, STORE_CALC_DATA, "heading", [final_heading, speed_time])
+
+            geofence_etas = {}
+            for geofence_name in zones:
+                geofence = zones[geofence_name]
+                eta = time_to_enter_geofence(current, final_heading, final_speed, geofence["coordinates"],
+                                             geofence["warn_time"])
+                geofence_etas[geofence_name] = eta
+
+            print("Geofence ETAs:", geofence_etas)  # TODO: Add actual use of the geofence ETAs
+
+
+def check_for_old_planes(current_time):
+    global planes
+    old_planes = []
+    for plane in planes:
+        last_packet_relative_time_ago = current_time - planes[plane]['internal']['last_update']
+        if last_packet_relative_time_ago > REMEMBER_PLANES:
+            old_planes.append(plane)
+    return old_planes
+
+
+def process_old_planes(old_planes):
+    for plane in old_planes:
+        print(len(planes) - 1, planes[plane])
+        del planes[plane]
+
+
+def time_to_enter_geofence(plane_position, heading, speed, geofence_coordinates, max_time):
+    # Create a shapely Polygon object from the geofence coordinates
+    geofence_polygon = Polygon(geofence_coordinates)
+
+    # Check if the current position is inside the geofence
+    if geofence_polygon.contains(Point(plane_position)):
+        return 0  # The plane is already inside the geofence
+
+    verified = False
+    for point in geofence_polygon.exterior.coords:
+        general_direction = calculate_heading(plane_position, point)
+        # print(general_direction, heading)
+        if abs(heading - general_direction) < POINT_ACCURACY_THRESHOLD_DEG:
+            verified = True
+
+    if verified is False:
+        print("not verified")
+        return math.inf
+
+    time_step = 0.1
+    current_time = time_step
+    while True:
+        if current_time > max_time:
+            return math.inf
+        destination = (
+            geopy.distance.geodesic(kilometers=time_step * speed / 3600)
+            .destination(plane_position, heading))
+
+        point = Point(destination.latitude, destination.longitude)
+        if geofence_polygon.contains(point):
+            return current_time
+        current_time += time_step
+
+
+planes = {}
+processed_messages = 0
+while True:
+    check_generator_thread()
+    processed_messages += process_messages(signal_generator.message_queue[:])
+    calculate()
+    signal_generator.message_queue = []  # Reset the messsages
+    old = check_for_old_planes(time.time())
+    print(planes)  # Print all generated plane data
+    process_old_planes(old)
     time.sleep(0.5)
